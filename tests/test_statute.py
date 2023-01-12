e9c87a62d36973a140329a2dfc3921fffa613f91
justmars <39538958+justmars@users.noreply.github.com>
2023-01-12 23:17:04 +0800

    feat: revised generation; pre-commit lint

--- tests/test_statute.py
+++ tests/test_statute.py
@@ -29,42 +29,42 @@ def test_ra_11716_unit_fts(ra_11716_obj):
         {
             "material_path": "1.1.",
             "statute_id": "ra-11716-april-29-2022",
-            "unit_text": "Section 2 of Republic Act No. 7938, as amended by Republic Act No. 10677, is hereby amended to read as follows:",
+            "unit_text": "Section 2 of Republic Act No. 7938, as amended by Republic Act No. 10677, is hereby amended to read as follows:",  # noqa: E501
         },
         {
             "material_path": "1.1.1.1.",
             "statute_id": "ra-11716-april-29-2022",
-            "unit_text": "The health care services and facilities of the Northern Mindanao Medical Center (NMMC) shall be upgraded to conform with and be commensurate to the bed capacity increase from six hundred (600) to one thousand two hundred (1,200) beds.",
+            "unit_text": "The health care services and facilities of the Northern Mindanao Medical Center (NMMC) shall be upgraded to conform with and be commensurate to the bed capacity increase from six hundred (600) to one thousand two hundred (1,200) beds.",  # noqa: E501
         },
         {
             "material_path": "1.1.1.2.",
             "statute_id": "ra-11716-april-29-2022",
-            "unit_text": "Any future increase in bed capacity shall be consistent with the hospital development plan which shall be prepared by the Department of Health (DOH).",
+            "unit_text": "Any future increase in bed capacity shall be consistent with the hospital development plan which shall be prepared by the Department of Health (DOH).",  # noqa: E501
         },
         {
             "material_path": "1.2.",
             "statute_id": "ra-11716-april-29-2022",
-            "unit_text": "Section 3 of the same Act is hereby amended to read as follows:",
+            "unit_text": "Section 3 of the same Act is hereby amended to read as follows:",  # noqa: E501
         },
         {
             "material_path": "1.2.1.",
             "statute_id": "ra-11716-april-29-2022",
-            "unit_text": "The number of medical personnel, the budget for personnel services and maintenance\nand other operating expenses of the NMMC shall correspondingly be increased. The\nSecretary of the DOH shall, in coordination with the Secretary of the Department\nof Budget and Management and the Chairperson of the Civil Service Commission,\nand after consultation with the Chief of NMMC, determine the additional plantilla\npositions to be created pursuant to this Act.",
+            "unit_text": "The number of medical personnel, the budget for personnel services and maintenance\nand other operating expenses of the NMMC shall correspondingly be increased. The\nSecretary of the DOH shall, in coordination with the Secretary of the Department\nof Budget and Management and the Chairperson of the Civil Service Commission,\nand after consultation with the Chief of NMMC, determine the additional plantilla\npositions to be created pursuant to this Act.",  # noqa: E501
         },
         {
             "material_path": "1.3.",
             "statute_id": "ra-11716-april-29-2022",
-            "unit_text": "Section 4 of the same Act is hereby amended to read as follows:",
+            "unit_text": "Section 4 of the same Act is hereby amended to read as follows:",  # noqa: E501
         },
         {
             "material_path": "1.3.1.",
             "statute_id": "ra-11716-april-29-2022",
-            "unit_text": "The increase in bed capacity of the NMMC fro six hundred (600) to one thousand\ntwo hundred (1,200) beds shall be implemented immediately upon approval of this\nAct.",
+            "unit_text": "The increase in bed capacity of the NMMC fro six hundred (600) to one thousand\ntwo hundred (1,200) beds shall be implemented immediately upon approval of this\nAct.",  # noqa: E501
         },
         {
             "material_path": "1.4.",
             "statute_id": "ra-11716-april-29-2022",
-            "unit_text": "This Act shall take effect fifteen (15) days after its publication in the *Official Gazette* or in a newspaper of general circulation.",
+            "unit_text": "This Act shall take effect fifteen (15) days after its publication in the *Official Gazette* or in a newspaper of general circulation.",  # noqa: E501
         },
     ]
 
@@ -79,7 +79,7 @@ def test_ra_11716_titles(ra_11716_obj):
         {
             "statute_id": "ra-11716-april-29-2022",
             "category": "official",
-            "text": "An Act Increasing The Bed Capacity Of The Northern Mindanao Medical Center In Cagayan De Oro City, Amending For The Purpose Republic Act No. 7938, As Amended By Republic Act No. 10677, And Appropriating Funds Therefor",
+            "text": "An Act Increasing The Bed Capacity Of The Northern Mindanao Medical Center In Cagayan De Oro City, Amending For The Purpose Republic Act No. 7938, As Amended By Republic Act No. 10677, And Appropriating Funds Therefor",  # noqa: E501
         },
     ]
 
@@ -110,7 +110,7 @@ def test_ra_11716_material_paths(ra_11716_obj):
         },
         {
             "item": "Section 1",
-            "content": "Section 2 of Republic Act No. 7938, as amended by Republic Act No. 10677, is hereby amended to read as follows:",
+            "content": "Section 2 of Republic Act No. 7938, as amended by Republic Act No. 10677, is hereby amended to read as follows:",  # noqa: E501
             "material_path": "1.1.",
             "statute_id": "ra-11716-april-29-2022",
         },
@@ -121,43 +121,43 @@ def test_ra_11716_material_paths(ra_11716_obj):
         },
         {
             "item": "Paragraph 1",
-            "content": "The health care services and facilities of the Northern Mindanao Medical Center (NMMC) shall be upgraded to conform with and be commensurate to the bed capacity increase from six hundred (600) to one thousand two hundred (1,200) beds.",
+            "content": "The health care services and facilities of the Northern Mindanao Medical Center (NMMC) shall be upgraded to conform with and be commensurate to the bed capacity increase from six hundred (600) to one thousand two hundred (1,200) beds.",  # noqa: E501
             "material_path": "1.1.1.1.",
             "statute_id": "ra-11716-april-29-2022",
         },
         {
             "item": "Paragraph 2",
-            "content": "Any future increase in bed capacity shall be consistent with the hospital development plan which shall be prepared by the Department of Health (DOH).",
+            "content": "Any future increase in bed capacity shall be consistent with the hospital development plan which shall be prepared by the Department of Health (DOH).",  # noqa: E501
             "material_path": "1.1.1.2.",
             "statute_id": "ra-11716-april-29-2022",
         },
         {
             "item": "Section 2",
-            "content": "Section 3 of the same Act is hereby amended to read as follows:",
+            "content": "Section 3 of the same Act is hereby amended to read as follows:",  # noqa: E501
             "material_path": "1.2.",
             "statute_id": "ra-11716-april-29-2022",
         },
         {
             "item": "Section 3",
-            "content": "The number of medical personnel, the budget for personnel services and maintenance\nand other operating expenses of the NMMC shall correspondingly be increased. The\nSecretary of the DOH shall, in coordination with the Secretary of the Department\nof Budget and Management and the Chairperson of the Civil Service Commission,\nand after consultation with the Chief of NMMC, determine the additional plantilla\npositions to be created pursuant to this Act.",
+            "content": "The number of medical personnel, the budget for personnel services and maintenance\nand other operating expenses of the NMMC shall correspondingly be increased. The\nSecretary of the DOH shall, in coordination with the Secretary of the Department\nof Budget and Management and the Chairperson of the Civil Service Commission,\nand after consultation with the Chief of NMMC, determine the additional plantilla\npositions to be created pursuant to this Act.",  # noqa: E501
             "material_path": "1.2.1.",
             "statute_id": "ra-11716-april-29-2022",
         },
         {
             "item": "Section 3",
-            "content": "Section 4 of the same Act is hereby amended to read as follows:",
+            "content": "Section 4 of the same Act is hereby amended to read as follows:",  # noqa: E501
             "material_path": "1.3.",
             "statute_id": "ra-11716-april-29-2022",
         },
         {
             "item": "Section 4",
-            "content": "The increase in bed capacity of the NMMC fro six hundred (600) to one thousand\ntwo hundred (1,200) beds shall be implemented immediately upon approval of this\nAct.",
+            "content": "The increase in bed capacity of the NMMC fro six hundred (600) to one thousand\ntwo hundred (1,200) beds shall be implemented immediately upon approval of this\nAct.",  # noqa: E501
             "material_path": "1.3.1.",
             "statute_id": "ra-11716-april-29-2022",
         },
         {
             "item": "Section 4",
-            "content": "This Act shall take effect fifteen (15) days after its publication in the *Official Gazette* or in a newspaper of general circulation.",
+            "content": "This Act shall take effect fifteen (15) days after its publication in the *Official Gazette* or in a newspaper of general circulation.",  # noqa: E501
             "material_path": "1.4.",
             "statute_id": "ra-11716-april-29-2022",
         },
